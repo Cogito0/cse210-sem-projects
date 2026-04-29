87929024e9ef59decfecc8cfd27c71cdb0918b04
Michael Kelsey <mihuke9@gmail.com>
2026-04-29 01:11:33 -0600

    Completed Prep Assignments 1, 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string given = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string family = Console.ReadLine();
+        Console.WriteLine($"Your name is {family}, {given} {family}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade in terms of percent?");
+        string grade = Console.ReadLine();
+        int g = int.Parse(grade);
+        string letterg = "E";
+
+        if (g >= 90)
+        {
+            letterg = "A";
+        }
+        else if (g >= 80)
+        {
+            letterg = "B";
+        }
+        else if (g >= 70)
+        {
+            letterg = "C";
+        }
+        else if (g >= 60)
+        {
+            letterg = "D";
+        }
+        else
+        {
+            letterg = "F";
+        }
+
+        Console.WriteLine($"Your letter grade for this class is a(n) {letterg}7");
+
+        if (g > 70)
+        {
+            Console.WriteLine("Terrific! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass, but you get another chance.");
+        }
     }
 }
