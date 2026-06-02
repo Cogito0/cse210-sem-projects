fba06d9808827028ca7ee5625b952db5e45d78e4
Michael Kelsey <mihuke9@gmail.com>
2026-06-02 16:15:01 -0600

    Complete and Test Prepare Assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment paper1 = new MathAssignment("Michael Kelsey", "Synthetic Division", "5.12", "39-49");
+        WritingAssignment paper2 = new WritingAssignment("Michael Kelsey", "Poetry in Religion", "How to analyze chiasmus in the Bible");
+
+        Console.WriteLine(paper1.GetSummary());
+        Console.WriteLine(paper1.GetHomeworkList());
+ 
+        Console.WriteLine(paper2.GetSummary());
+        Console.WriteLine(paper2.GetWritingInformation());
     }
 }
